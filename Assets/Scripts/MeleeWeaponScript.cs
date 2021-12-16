696122cb6747637d1ef81d0449bee0746e0c7362
Phillips Albright <55517109+phillipsalbright@users.noreply.github.com>
2021-12-16 00:20:26 -0500

    Partial implementation of split screen in the test scene

--- Assets/Scripts/MeleeWeaponScript.cs
+++ Assets/Scripts/MeleeWeaponScript.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class MeleeWeaponScript : MonoBehaviour
 {
     private float damage = 10f;
-    private float range = 10f;
-    private float knockbackForce = 10f;
+    private float range = 4;
+    private float knockbackForce = 12;
     private Animator meleeAnimator;
     private float nextTimeToAttack = 0f;
+    private float attackRate = 1f;
     /** Position of the crosshair on screen, used to detect if the attack should "hit" or not */
     public GameObject raycastPosition;
     // Start is called before the first frame update
@@ -20,12 +21,29 @@ public class MeleeWeaponScript : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToAttack)
         {
-
+            nextTimeToAttack = Time.time + 1f / attackRate;
+            Attack();
         }
     }
 
     private void Attack()
     {
-        
+        meleeAnimator.SetTrigger("Attack");
+        RaycastHit hit;
+        if (Physics.Raycast(raycastPosition.transform.position, raycastPosition.transform.forward, out hit, range))
+        {
+            PlayerHealth target = hit.transform.GetComponent<PlayerHealth>();
+            if (target != null)
+            {
+                Debug.Log("frog1");
+                target.TakeDamage(damage);
+            }
+            if (hit.rigidbody != null)
+            {
+                Debug.Log("frog2");
+                float multiplier = (target.health / 25) + 1f;
+                hit.rigidbody.AddForce(-hit.normal * knockbackForce, ForceMode.Impulse);
+            }
+        }
     }
 }
